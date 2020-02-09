0cbe5311517a2dc1208529b301473d39188a3489
mzq737 <60275847+mzq737@users.noreply.github.com>
2020-02-09 08:04:09 -0500

    Update Complex.java

--- 2020S/SimpleClasses/src/Complex.java
+++ 2020S/SimpleClasses/src/Complex.java
@@ -3,11 +3,29 @@
  * @author dkruger
  */
 public class Complex {
-    public static void main(String[] args) {
-        Complex a = new Complex(1.5, 2.1);
+	public final double real;
+	public final double imag;
+	
+	public Complex(double real, double imag) {
+		this.real = real;
+		this.imag = imag;
+	}
+	
+	public Complex add(Complex a) {
+		return new Complex(real + a.real, imag + a.imag);
+	}
+	
+	public String toString() {
+		return "(" + real + "," + imag + ")";
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Complex a = new Complex(1.5, 2.1);
         Complex b = new Complex(-1.2, 3.3);
         Complex c = a.add(b); // should be (0.3, 5.4)
-        System.out.println(a + " + " + b + " = " + c);
+        System.out.println(a.toString() + " + " + b.toString() + " = " + c.toString());
         // (1.5,2.1) + (-1.2,3.3) = (0.3,5.4)
-    }
+	}
+
 }
